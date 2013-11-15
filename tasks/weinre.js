f4a544081941e355ca5653e6dcb5eb682ef137c1
ChrisWren <cthewren@gmail.com>
2013-11-14 20:33:48 -0800

    Use require.resolve to determine weinre path

--- tasks/weinre.js
+++ tasks/weinre.js
@@ -12,7 +12,7 @@ module.exports = function (grunt) {
 
     var options = this.options();
     var done = this.async();
-    var args = [__dirname + '/../node_modules/weinre/weinre'];
+    var args = [require.resolve('weinre/weinre')];
 
     [
       'httpPort',
@@ -37,13 +37,7 @@ module.exports = function (grunt) {
     },
     function (error) {
       if (error) {
-        grunt.fail.fatal('weinre must be installed as a local dependency of grunt-weinre.\n\n' +
-
-                         'Run the following command:\n' +
-                         'rm -rf node_modules/weinre\n\n' +
-
-                         'Then run:\n' +
-                         'npm install grunt-weinre --save-dev');
+        grunt.fail.fatal(error);
       }
       done();
     });
